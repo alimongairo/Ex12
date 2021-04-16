a1b86112030eea53d2883285b492956cb111835a
Irina <pasmanikirina29@gmail.com>
2021-04-16 12:13:45 +0300

    file additition

---
+++ src/TimedDoor.cpp
@@ -0,0 +1,32 @@
+// Copyright 2021 by idpass
+#include <iostream>
+#include "TimedDoor.h"
+
+//TIMER CLIENT
+//virtual void Timeout() = 0;
+
+//DOOR
+//virtual void lock() = 0;
+//virtual void unlock() = 0;
+//virtual bool isDoorOpened() = 0;
+
+//DOOR TIMER ADAPTER
+//TimedDoor& door;
+//explicit DoorTimerAdapter(TimedDoor&);
+//void Timeout();
+
+//TIMED DOOR
+//DoorTimerAdapter* adapter;
+//int iTimeout;
+//bool opened;
+//explicit TimedDoor(int);
+//bool isDoorOpened();
+//void unlock();
+//void lock();
+//void DoorTimeOut();
+//void throwState();
+
+//TIMER
+//TimerClient* client;
+//void sleep(int);
+//void tregister(int, TimerClient*);
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
-// Copyright 2021 GHA Test Team
-#include "TimedDoor.h"
+// Copyright 2021 by idpas
 #include <iostream>
+#include "TimedDoor.h"
 
 int main() {
   TimedDoor tDoor(5);
