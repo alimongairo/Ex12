70ac15e8137ef51425e51ab2084f55191ff62aaf
Irina <pasmanikirina29@gmail.com>
2021-04-16 12:39:18 +0300

    TimedDoor functions

--- src/TimedDoor.cpp
+++ src/TimedDoor.cpp
@@ -1,32 +1,51 @@
 // Copyright 2021 by idpass
-#include <iostream>
 #include "TimedDoor.h"
+#include <iostream>
 
-//TIMER CLIENT
-//virtual void Timeout() = 0;
+// TIMER CLIENT
+// virtual void Timeout() = 0;
 
-//DOOR
-//virtual void lock() = 0;
-//virtual void unlock() = 0;
-//virtual bool isDoorOpened() = 0;
+// DOOR
+// virtual void lock() = 0;
+// virtual void unlock() = 0;
+// virtual bool isDoorOpened() = 0;
 
-//DOOR TIMER ADAPTER
-//TimedDoor& door;
-//explicit DoorTimerAdapter(TimedDoor&);
-//void Timeout();
+// DOOR TIMER ADAPTER
+// TimedDoor& door;
+// explicit DoorTimerAdapter(TimedDoor&);
+// void Timeout();
 
-//TIMED DOOR
-//DoorTimerAdapter* adapter;
-//int iTimeout;
-//bool opened;
-//explicit TimedDoor(int);
-//bool isDoorOpened();
-//void unlock();
-//void lock();
-//void DoorTimeOut();
-//void throwState();
+/* TIMED DOOR */
 
-//TIMER
-//TimerClient* client;
-//void sleep(int);
-//void tregister(int, TimerClient*);
+TimedDoor::TimedDoor(int t)
+    : iTimeout(t), opened(false), adapter(new DoorTimerAdapter(*this)) {}
+
+bool TimedDoor::isDoorOpened() {
+	return opened;
+}
+
+void TimedDoor::unlock() {
+  opened = true;
+  adapter->Timeout();
+}
+
+void TimedDoor::lock() {
+	opened = false;
+}
+
+void TimedDoor::DoorTimeOut() {
+	throw std::string("Close the door!");
+}
+
+void TimedDoor::throwState() {
+  if (opened == true) {
+    throw std::string("Opened");
+  } else {
+    throw std::string("Closed");
+  }
+}
+
+// TIMER
+// TimerClient* client;
+// void sleep(int);
+// void tregister(int, TimerClient*);
